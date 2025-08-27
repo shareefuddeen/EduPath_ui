112f7156486dd5f00346ddcdb816ac892376b544
shareefuddeen <awalsharifpz18@gmail.com>
2025-08-27 22:41:54 +0000

    smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Navbar from "./Components/Navbar";
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Home from "./Pages/Home";
@@ -10,6 +11,24 @@ import ResetPassword from "./Pages/reset_password";
 import ResetPasswordConfirm from "./Pages/Reset_password_confirm";
 
 function App() {
+   useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
+      smooth: true,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
   return (
     <>
       <BrowserRouter>
